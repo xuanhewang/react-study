7066bc448924aab8eca091522ca801058e3ed9e5
xuanhewang <811300735@qq.com>
2016-11-12 09:33:13 +0800

    react-study

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import List from './Component/List.js'
+
+class App extends React.Component {
+
+  render () {
+    return(
+      <div>
+        <List />
+      </div>
+
+    )
+  }
+}
+
+
+
+
+export default App;
